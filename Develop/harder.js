3b6ec852ca9f4b5430ff8790a231be55dbad8acf
Jodi Rhoades <jo_kawgirl@yahoo.com>
2019-12-28 18:10:33 -0500

    harder version html addtions

--- Develop/harder.js
+++ Develop/harder.js
@@ -32,19 +32,26 @@ async function promptUser() {
 
         const profileData = await axios.get(`https://api.github.com/users/${github}`)
 
-        const pdfText = generateHTML(profileData.data, favColor);
+        profileData.data.color = favColor;
+
+        const pdfText = generateHTML(profileData.data);
         console.log(pdfText)
 
+        fs.writeFile("test.HTML", pdfText, err => {
+            if (err) throw err
+            console.log("success")
+        })
         pdf.create(pdfText, options).toFile('generateHTML.pdf', function (err, res) {
             if (err) return console.log(err);
             console.log(res);
-        });
+        });        
 
-        catch(err) {
-            console.log(err)
-        }
+    
 
     } 
+    catch(err) {
+        console.log(err)
+    }
    };
 
 function makepdf(userInfo, color) {
@@ -52,14 +59,14 @@ function makepdf(userInfo, color) {
 <img src="${userInfo.avatar_url}" alt="coder photo" height="75"><br>
 Name: ${userInfo.name}
 Username: ${userInfo.login}  
-Location Map: [google link](${'https://www.google.com/maps/place/Charlotte,+NC/@35.2030728,-80.9799136,11z/data=!3m1!4b1!4m5!3m4!1s0x88541fc4fc381a81:0x884650e6bf43d164!8m2!3d35.2270869!4d-80.8431267'})  
+
 Profile URL: [profile link](${userInfo.html_url})  
-Blog URL: [blog link] ${userInfo.html_url}
+Blog URL: [blog link] ${userInfo.blog}
 Bio: ${userInfo.bio}  
 Repo URL: [repo link](${userInfo.repos_url})  
 Public Repos:  ${userInfo.public_repos}  
 Followers: ${userInfo.followers}  
-GitHub stars: ${userInfo.watchers_count}
+
 Following: ${userInfo.following}  
 `
 }
